1845568a6801ae6fc34abde02119252315456fc9
geoffreyfarel <geoffreyfarrel67@gmail.com>
2026-04-10 14:19:52 +0800

    feat: AuthBackend

---
+++ app/actions/login.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@/utils/supabase/server';
+
+export default async function loginUser(
+  formData: FormData,
+): Promise<{ success?: boolean; error?: string }> {
+  const supabase = await createClient();
+
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) return { error: error.message };
+
+  return { success: true };
+}
--- app/actions/register.ts
+++ app/actions/register.ts
@@ -1,27 +1,46 @@
 'use server';
 
-import { createClient } from '@supabase/supabase-js';
-import bcrypt from 'bcrypt';
+import z from 'zod';
+
+import { createClient } from '@/utils/supabase/server';
+
+const registerSchema = z.object({
+  username: z
+    .string()
+    .min(3, 'Username must be at least 3 characters long.')
+    .max(20, 'Username must be at most 20 characters long.'),
+  email: z.email('Invalid email address.'),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters long.')
+    .max(16, 'Password must be at most 16 characters long.')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter.')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter.')
+    .regex(/[0-9]/, 'Password must contain at least one number.'),
+});
+
 export async function registerUser(
   formData: FormData,
-): Promise<{ error?: string; success?: boolean }> {
-  const username = formData.get('username') as string;
+): Promise<{ success?: boolean; error?: string }> {
+  const email = formData.get('email') as string;
   const password = formData.get('password') as string;
+  const username = formData.get('username') as string;
 
-  const saltRounds = 10;
-  const hash = await bcrypt.hash(password, saltRounds);
+  if (!email || !password || !username) {
+    return { success: false, error: 'All fields are required.' };
+  }
 
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  );
+  const supabase = await createClient();
 
-  const { error } = await supabase.from('users').insert([
-    {
-      username,
-      password: hash,
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        display_name: username,
+      },
     },
-  ]);
+  });
 
   if (error) return { error: error.message };
 
